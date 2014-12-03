8dfc5c02d7776192eded6819b2b278193681a8f2
MRodriguezz <marcos_roag@hotmail.com>
2014-12-03 10:01:13 -0600

    Numero Factorial

--- application.py
+++ application.py
@@ -1,3 +1,16 @@
-# Aqui escribe tu codigo
+#-*- coding: utf-8-*-
+#******************************************************************
+#********************Número Factoria*******************************
+#******************************************************************
+print "NUMERO FACTORIAL RECURSIVO"
+#******************************************************************
+def factorial(n):  
+	if n == 0 or n == 1:
+		resultado = 1
+	elif n > 1:
+		resultado = n*factorial(n-1)
+	return resultado
 
-print "Hello World"
+numero=int(input("Ingrese Numero: "))
+print "Su Factorial es: %s"%factorial(numero)
+
